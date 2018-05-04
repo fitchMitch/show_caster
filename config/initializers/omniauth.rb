d924f3b253fa7a546ae12828be60a569827d42f6
Etienne WEIL <weil.etienne@hotmail.fr>
2018-05-04 15:09:56 +0200

    omniauth initializer adds redirect_uri

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,16 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  extra = {
+    access_type: "offline",
+    prompt: "consent",
+    select_account: true,
+    scope: 'userinfo.email, calendar, drive'
+  }
+  extra[:redirect_uri] = "http://etienneweil.fr:3200/auth/google_oauth2/callback" if Rails.env.production?
+
   provider :google_oauth2,
     ENV.fetch("GOOGLE_CLIENT_ID"),
-    ENV.fetch("GOOGLE_CLIENT_SECRETS"),{
-      access_type: "offline",
-      prompt: "consent",
-      select_account: true,
-      scope: 'userinfo.email, calendar, drive'
-    }
+    ENV.fetch("GOOGLE_CLIENT_SECRETS"),
+    extra
 end
 
 OmniAuth.config.on_failure = SessionsController.action(:oauth_failure)
