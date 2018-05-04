ae2098dc58ecf5c140d693354e6f0e8a8f31023b
Etienne WEIL <weil.etienne@hotmail.fr>
2018-05-04 10:20:28 +0200

    using ENV.fetch ep.2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,13 +1,14 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRETS'],{
-    access_type: "offline",
-    prompt: "consent",
-    select_account: true,
-    scope: 'userinfo.email,calendar'
-  }
+  provider :google_oauth2,
+    ENV.fetch("GOOGLE_CLIENT_ID"),
+    ENV.fetch("GOOGLE_CLIENT_SECRETS)",{
+      access_type: "offline",
+      prompt: "consent",
+      select_account: true,
+      scope: 'userinfo.email, calendar, drive'
+    }
 end
 
 OmniAuth.config.on_failure = SessionsController.action(:oauth_failure)
 
-# TODO:
-# OmniAuth.config.full_host = Rails.env.production? ? 'https://etienneweil.fr:3200' : 'http://localhost:3000'
+OmniAuth.config.full_host = Rails.env.production? ? 'https://etienneweil.fr:3200' : 'http://localhost:3000'
