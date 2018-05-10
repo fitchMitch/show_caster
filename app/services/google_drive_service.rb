66e0ecd0147c42be01543945f331232cc694b2c1
Etienne WEIL <weil.etienne@hotmail.fr>
2018-05-10 09:31:04 +0200

    Last try with GDrive API

--- app/services/google_drive_service.rb
+++ app/services/google_drive_service.rb
@@ -28,6 +28,21 @@ class GoogleDriveService
     @service
   end
   def file_list
-    @service.list_files.files
+    # @service.list_files.files
+    # Photos : https://drive.google.com/open?id=0B3Djw_z2qYLeRHFQMHlCd0VQc28
+    # photo_folder_id = "0B3Djw_z2qYLedURtLVV4Mm9RVjA" # This is the Photos id , I think
+    # photo_folder_id = "0B3Djw_z2qYLea0NyNmQwZnFqUWs" # This is the Photos id , I think
+    # q = "mimeType='image/jpeg' and sharedWithMe=true and '#{photo_folder_id}' in parents"
+    # q = "sharedWithMe=true and mimeType='application/vnd.google-apps.folder' and '0B3Djw_z2qYLea0NyNmQwZnFqUWs' in parents"
+    q = "sharedWithMe=true and mimeType='application/vnd.google-apps.folder'"
+    files =   @service.fetch_all(items: :files) do |page_token|
+        @service.list_files(
+          q: q,
+          spaces: 'drive',
+          fields: 'nextPageToken, files(id, name, parents)',
+          page_token: page_token)
+    end
+
+
   end
 end
