ef0420eee655774c8911a0846291c94ebc284471
Etienne WEIL <weil.etienne@hotmail.fr>
2018-06-24 22:28:24 +0200

    annotate doc

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -14,6 +14,7 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  title      :string
+#  type       :string           default("Performance")
 #
 
 FactoryBot.define do
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :integer          not null, primary key
+#  event_date :datetime
+#  duration   :integer
+#  progress   :integer          default("draft")
+#  note       :text
+#  user_id    :integer
+#  theater_id :integer
+#  fk         :string
+#  provider   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  title      :string
+#  type       :string           default("Performance")
+#
+
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
