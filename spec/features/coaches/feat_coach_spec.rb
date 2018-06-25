2d0f7074b732ecf3a19afad34dab8829c348401c
Etienne WEIL <weil.etienne@hotmail.fr>
2018-06-25 16:50:24 +0200

    specs features update

---
+++ spec/features/coaches/feat_coach_spec.rb
@@ -0,0 +1,97 @@
+require 'rails_helper'
+
+RSpec.feature  "Coach" do
+  feature "as a registered admin" do
+    given! (:admin) { create(:user, :admin, :registered, lastname: "ADMIN") }
+    feature "visiting INDEX" do
+      background :each do
+        log_in admin
+        visit coaches_path
+      end
+
+      scenario "should list the coach's band members" do
+        expect(page.body).to have_selector("h2", text: I18n.t("coaches.list"))
+      end
+
+      scenario "should propose an add button" do
+        # expect(page.body).to have_button text:I18n.t("coaches.new")
+        # click_button(I18n.t("coaches.new"))
+        visit new_coach_path
+        expect(page.body).to have_selector("h2", text: I18n.t("coaches.new"))
+      end
+    end
+
+    feature "visiting NEW and CREATE" do
+      background :each do
+        log_in admin
+        visit new_coach_path
+        within "#coach_new" do
+          fill_in "coach_firstname", with: "Edouard IV"
+          fill_in "coach_lastname", with: "berurier"
+          fill_in "coach_email", with: "bg@fra.fr"
+          fill_in "coach_cell_phone_nr", with: "0123654789"
+          fill_in "coach_note", with: "fracan"
+        end
+        click_button(I18n.t("helpers.submit.coach.create"))
+      end
+
+      scenario "it shall create a Coach" do
+        coach = Coach.last
+        expect(page.body).to have_content I18n.t("coaches.save_success")
+        expect(page.body).to have_content coach.firstname
+        expect(page.body).to have_content coach.lastname
+        expect(page.body).to have_content coach.email
+        expect(page.body).to have_content "01 23 65 47 89"
+        expect(page.body).to have_content coach.note
+      end
+    end
+
+    feature "visiting UPDATE" do
+      background :each do
+        log_in admin
+        coach = create(:coach)
+        visit edit_coach_path(coach)
+        within "#coach_new" do
+          fill_in "coach_firstname", with: "Edouard IV"
+          fill_in "coach_lastname", with: "berurier"
+          fill_in "coach_email", with: "bg@fra.fr"
+          fill_in "coach_cell_phone_nr", with: "0123654789"
+          fill_in "coach_note", with: "fracan"
+        end
+        click_button(I18n.t("helpers.submit.coach.update"))
+      end
+
+      scenario "it shall create a Coach" do
+        coach = Coach.last
+        expect(page.body).to have_content I18n.t("coaches.update_success")
+        expect(page.body).to have_content coach.firstname
+        expect(page.body).to have_content coach.lastname
+        expect(page.body).to have_content coach.email
+        expect(page.body).to have_content "01 23 65 47 89"
+        expect(page.body).to have_content coach.note
+      end
+    end
+
+    feature "visiting UPDATE fails" do
+      background :each do
+        log_in admin
+        coach = create(:coach)
+        visit edit_coach_path(coach)
+        within "#coach_new" do
+          fill_in "coach_firstname", with: ""
+          fill_in "coach_lastname", with: "berurier"
+          fill_in "coach_email", with: "bg@fra.fr"
+          fill_in "coach_cell_phone_nr", with: "0123654789"
+          fill_in "coach_note", with: "fracan"
+        end
+        click_button(I18n.t("helpers.submit.coach.update"))
+      end
+
+      scenario "it shall create a Coach" do
+        coach = Coach.last
+        expect(page.body).to have_content (I18n.t("coaches.save_fails"))
+        expect(page.body).to have_selector("h2", text: I18n.t("edit"))
+      end
+    end
+  end
+end
--- spec/features/events/feat_event_spec.rb
+++ spec/features/events/feat_event_spec.rb
@@ -4,85 +4,85 @@ require 'vcr'
 RSpec.feature  "Events | " do
   feature "as a registered admin" do
     given (:admin) { create(:user, :admin, :registered, lastname: "ADMIN") }
-    given (:event) { create(:performance) }
-    given (:other_event) { create(:other_event) }
-    given! (:event_w) { create(:performance_with_actors) }
-    given! (:theater_w) { create(:theater_with_event)}
+    given (:performance) { create(:performance) }
+    given (:other_performance) { create(:performance) }
+    given! (:performance_w) { create(:performance_with_actors) }
+    given! (:theater_w) { create(:theater_with_performance)}
 
 
     feature "visiting INDEX" do
       background :each do
         log_in admin
-        visit events_path
+        visit performances_path
       end
 
-      scenario "should list the events" do
-        expect(page.body).to have_selector("h2", text: I18n.t("events.list"))
+      scenario "should list the performances" do
+        expect(page.body).to have_selector("h2", text: I18n.t("performances.list"))
       end
 
       scenario "should propose an add button" do
-        visit new_event_path
-        expect(page.body).to have_selector("h2", text: I18n.t("events.new"))
+        visit new_performance_path
+        expect(page.body).to have_selector("h2", text: I18n.t("performances.new"))
       end
     end
 
     feature "visiting NEW and CREATE" do
       background :each do
         log_in admin
-        visit new_event_path
+        visit new_performance_path
         last_theater_id = Theater.all.last.id
         page.find('#event_theater_id').find(:xpath, 'option[1]').select_option
         page.find('#event_duration').find(:xpath, 'option[3]').select_option
-        click_button(I18n.t("helpers.submit.event.create"))
+        click_button(I18n.t("helpers.submit.performance.create"))
       end
 
       scenario "it shall create a Event", :vcr do
-        expect(page.body).to have_content I18n.t("events.created")
-        expect(page.body).to have_content Event::DURATIONS.rassoc(event.duration)
-        expect(page.body).to have_content event.progress_i18n
+        expect(page.body).to have_content I18n.t("performances.created")
+        expect(page.body).to have_content Event::DURATIONS.rassoc(performance.duration)
+        expect(page.body).to have_content performance.progress_i18n
       end
     end
 
     feature "visiting UPDATE" do
       background :each do
         log_in admin
-        visit edit_event_path(event_w)
+        visit edit_performance_path(performance_w)
         page.find('#event_theater_id').find(:xpath, 'option[1]').select_option
         page.find('#event_duration').find(:xpath, 'option[4]').select_option
-        click_button(I18n.t("helpers.submit.event.update"))
+        click_button(I18n.t("helpers.submit.performance.update"))
       end
 
       scenario "it shall update Events", :vcr do
-        expect(page.body).to have_content I18n.t("events.updated")
-        expect(page.body).to have_content Event::DURATIONS.rassoc(event.duration)
-        expect(page.body).to have_content event.progress_i18n
+        expect(page.body).to have_content I18n.t("performances.updated")
+        expect(page.body).to have_content Event::DURATIONS.rassoc(performance.duration)
+        expect(page.body).to have_content performance.progress_i18n
       end
     end
 
     feature "visiting UPDATE fails" do
       background :each do
         log_in admin
-        visit edit_event_path(event_w)
+        visit edit_performance_path(performance_w)
         page.find('#event_theater_id').find(:xpath, 'option[1]').select_option
-        click_button(I18n.t("helpers.submit.event.update"))
+        click_button(I18n.t("helpers.submit.performance.update"))
       end
 
       scenario "it shall not update Events" do
-        expect(page.body).to have_content (I18n.t("events.desynchronized"))
-        expect(page.body).to have_selector("h2", text: I18n.t("events.edit_title"))
+        expect(page.body).to have_content (I18n.t("performances.desynchronized"))
+        expect(page.body).to have_selector("h2", text: I18n.t("performances.edit_title"))
       end
     end
 
     feature "visiting DELETE fails" do
       background :each do
         log_in admin
-        visit events_path
+        visit performances_path
         page.find('.destroy', match: :first).click
       end
 
       scenario "it shall delete Events" ,:vcr do
-        expect(page.body).to have_content (I18n.t("events.destroyed"))
-        expect(page.body).to have_selector("h2", text: I18n.t("events.list"))
+        expect(page.body).to have_content (I18n.t("performances.destroyed"))
+        expect(page.body).to have_selector("h2", text: I18n.t("performances.list"))
       end
     end
   end
