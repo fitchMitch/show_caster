20fabeeeaf0f94a3cba68e485f31256b25a04c63
Etienne WEIL <weil.etienne@hotmail.fr>
2018-07-21 18:08:10 +0200

    polls and answers policies

---
+++ spec/policies/answer_policy_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+RSpec.describe AnswerPolicy do
+  subject { AnswerPolicy.new(user, answer)}
+  let!(:answer) { answer = FactoryBot.create(:answer) }
+
+  context "As a visitor" do
+    let!(:user) { nil }
+    it { is_expected.to     forbid_action(:index) }
+    it { is_expected.to     forbid_action(:show) }
+    it { is_expected.to     forbid_action(:edit) }
+    it { is_expected.to     forbid_action(:update) }
+    it { is_expected.to     forbid_action(:destroy) }
+  end
+
+  context "As a player" do
+    let!(:user) { FactoryBot.create(:user, :player,:registered) }
+
+    subject { AnswerPolicy.new(user, answer)}
+    it { is_expected.to      permit_action(:index) }
+    it { is_expected.to      permit_action(:show) }
+    it { is_expected.to      permit_action(:edit) }
+    it { is_expected.to      permit_action(:update) }
+    it { is_expected.to      forbid_action(:destroy) }
+  end
+
+  context "As an admin" do
+    let!(:user) { FactoryBot.create(:user, :admin,:registered) }
+
+    subject { AnswerPolicy.new(user, answer)}
+    it { is_expected.to     permit_action(:index) }
+    it { is_expected.to     permit_action(:show) }
+    it { is_expected.to     permit_action(:edit) }
+    it { is_expected.to     permit_action(:update) }
+    it { is_expected.to     permit_action(:destroy) }
+  end
+
+  context "As an admin_com" do
+    let!(:user) {FactoryBot.create(:user,:admin_com,:registered) }
+
+    subject { AnswerPolicy.new(user, answer)}
+    it { is_expected.to     permit_action(:index) }
+    it { is_expected.to     permit_action(:show) }
+    it { is_expected.to     permit_action(:edit) }
+    it { is_expected.to     permit_action(:update) }
+    it { is_expected.to     permit_action(:destroy) }
+  end
+end
---
+++ spec/policies/poll_date_policy_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+RSpec.describe PollDatePolicy do
+  subject { PollDatePolicy.new(user, poll)}
+  let!(:poll) { poll = FactoryBot.create(:poll_date) }
+
+  context "As a visitor" do
+    let!(:user) { nil }
+    it { is_expected.to     forbid_action(:index) }
+    it { is_expected.to     forbid_action(:show) }
+    it { is_expected.to     forbid_action(:edit) }
+    it { is_expected.to     forbid_action(:update) }
+    it { is_expected.to     forbid_action(:destroy) }
+  end
+
+  context "As a player" do
+    let!(:user) { FactoryBot.create(:user, :player,:registered) }
+
+    subject { PollDatePolicy.new(user, poll)}
+    it { is_expected.to      permit_action(:index) }
+    it { is_expected.to      permit_action(:show) }
+    it { is_expected.to      permit_action(:edit) }
+    it { is_expected.to      permit_action(:update) }
+    it { is_expected.to      forbid_action(:destroy) }
+  end
+
+  context "As an admin" do
+    let!(:user) { FactoryBot.create(:user, :admin,:registered) }
+
+    subject { PollDatePolicy.new(user, poll)}
+    it { is_expected.to     permit_action(:index) }
+    it { is_expected.to     permit_action(:show) }
+    it { is_expected.to     permit_action(:edit) }
+    it { is_expected.to     permit_action(:update) }
+    it { is_expected.to     permit_action(:destroy) }
+  end
+
+  context "As an admin_com" do
+    let!(:user) {FactoryBot.create(:user,:admin_com,:registered) }
+
+    subject { PollDatePolicy.new(user, poll)}
+    it { is_expected.to     permit_action(:index) }
+    it { is_expected.to     permit_action(:show) }
+    it { is_expected.to     permit_action(:edit) }
+    it { is_expected.to     permit_action(:update) }
+    it { is_expected.to     permit_action(:destroy) }
+  end
+end
