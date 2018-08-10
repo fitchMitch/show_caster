c6e4428fbb58d6e98732e034d289193f7a0f56d2
Etienne WEIL <weil.etienne@hotmail.fr>
2018-08-10 14:58:15 +0200

    hot fix 3 :/

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -5,12 +5,15 @@ class SplashController < ApplicationController
   def index
     @players_firstnames = []
     @next_performances = Event.performances.future_events.limit(5).includes(:actors)
-    @very_next_performance = @next_performances.first
-    @other_performances = @next_performances.drop(0)
-    @next_players = @very_next_performance.actors.where('stage_role = ?', 0)
-    @next_players.each do |player|
-      @players_firstnames << player.user.firstname
+    @very_next_performance = @next_performances.first || nil
+    
+    unless @very_next_performance.nil?
+      @next_players = @very_next_performance.actors.where('stage_role = ?', 0)
+      @next_players.each do |player|
+        @players_firstnames << player.user.firstname
+      end
     end
+    @other_performances = @next_performances.drop(0)
   end
 
   def signup
