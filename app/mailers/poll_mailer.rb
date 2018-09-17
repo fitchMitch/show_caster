f48291b03e1bafbbb716b4ad0733756900ac3bfa
Etienne WEIL <weil.etienne@hotmail.fr>
2018-09-17 08:44:27 +0200

    poll_mailer fix

--- app/mailers/poll_mailer.rb
+++ app/mailers/poll_mailer.rb
@@ -5,11 +5,15 @@ class PollMailer < ApplicationMailer
     @initiater = poll.owner.firstname
     @url = get_poll_url
     @final_call = poll.expiration_date
-    mail( to: Proc.new { User.company_mails }, subject: I18n.t("polls.mails.new_poll.subject", firstname: @initiater ) )
+    mail(
+      to: User.company_mails.join(','),
+      subject: I18n.t('polls.mails.new_poll.subject', firstname: @initiater)
+    )
   end
 
   private
-    def get_poll_url
-      "http://#{ get_site }/polls"
-    end
+
+  def get_poll_url
+    "http://#{get_site}/polls"
+  end
 end
