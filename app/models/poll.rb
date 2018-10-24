c17e4ac429fce9bee25aca37d88c1542262a4bb4
Etienne WEIL <weil.etienne@hotmail.fr>
2018-10-24 11:13:35 +0200

    rubocoping polls model

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -22,43 +22,40 @@ class Poll < ApplicationRecord
   # Relationships
   #-----------
   has_many :poll_opinions,
-    class_name: 'PollOpinion'
+           class_name: 'PollOpinion'
   has_many :poll_dates,
-    class_name: 'PollDate'
+           class_name: 'PollDate'
 
   belongs_to :owner,
-    class_name: 'User'
+             class_name: 'User'
 
   has_many :answers,
-    dependent: :destroy
+           dependent: :destroy
   accepts_nested_attributes_for :answers,
-    reject_if: :all_blank,
-    allow_destroy: true
+                                reject_if: :all_blank,
+                                allow_destroy: true
 
   # Validations
   #-----------
   validates :question,
-    presence: true,
-    length: { minimum: 5, maximum: 120 }
+            presence: true,
+            length: { minimum: 5, maximum: 120 }
   validates :expiration_date,
-    presence: true
+            presence: true
 
   # Scope
   #-----------
-  default_scope  -> { order('expiration_date ASC') }
+  default_scope -> { order('expiration_date ASC') }
   scope :opinion_polls, -> { where('type = ?', 'PollOpinion') }
-  scope :date_polls, -> { where('type = ?', 'PollDate')}
+  scope :date_polls, -> { where('type = ?', 'PollDate') }
   scope :passed_ordered, -> { unscoped.order('expiration_date DESC') }
-  # scope :found_by, -> (user) { where('user_id = ?', user_id) }
-  # scope :expecting_answer, -> { where(status: [:invited, :googled, :registered])}
   scope :expired, -> { where('expiration_date < ?', Time.zone.now) }
   scope :active, -> { where('expiration_date >= ?', Time.zone.now) }
-  # scope :expecting_answer, -> { where('expiration_date >= ?', Time.zone.now)} #
   # ------------------------
   # --    PUBLIC      ---
   # ------------------------
   def votes_count
-    Vote.where('poll_id = ?', self.id).group(:user).count.keys.size
+    Vote.where(poll_id: id).group(:user).count.keys.size
   end
 
   def poll_creation_mail
