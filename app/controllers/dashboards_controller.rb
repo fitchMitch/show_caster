ccb37ec561d8df36476b48c251a491431b349888
Etienne WEIL <weil.etienne@hotmail.fr>
2018-11-04 12:06:31 +0100

    dashboard complementary details

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -9,10 +9,7 @@ class DashboardsController < ApplicationController
       { role: 1, period_label: 'a_year', period_start_time: 1.year.ago },
       { role: 2, period_label: 'a_year', period_start_time: 1.year.ago }
     ]
-    indicators.each do |indic|
-      @dashboard.add Indicator.new indic
-    end
-
+    indicators.each { |indic| @dashboard.add Indicator.new indic }
     @periods = {
       three_month: 3.months.ago,
       six_month: 6.months.ago,
