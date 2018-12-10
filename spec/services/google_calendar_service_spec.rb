1b61fb963bf6ad7a2f7ad4c1a13fd1e196286a5c
Etienne WEIL <weil.etienne@hotmail.fr>
2018-12-10 23:00:13 +0100

    side work : specs on google service

--- spec/services/google_calendar_service_spec.rb
+++ spec/services/google_calendar_service_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 # require 'vcr'
-RSpec.describe 'GoogleCalendarService', type: :service do
+RSpec.describe GoogleCalendarService, type: :service do
   let!(:user) { create(:user, :registered, :admin) }
 
   describe '#existing_event?' do
+    subject { GoogleCalendarService.new(user) }
     let(:id) { double('id') }
-    let(:calendar) { double('calendar') }
+    # let(:calendar) { double('calendar') }
+    let(:client_service) { double(Google::APIClient::ClientSecrets) }
+    let(:google_api_calendar_service) { double(Google::Apis::CalendarV3::CalendarService) }
+    let(:signet_object) { double(Signet::OAuth2::Client) }
     let!(:company_calendar_id) { double('company_calendar_id') }
     before :each do
       mock_valid_auth_hash(user)
@@ -13,36 +17,38 @@ RSpec.describe 'GoogleCalendarService', type: :service do
       allow_any_instance_of(
         GoogleCalendarService
       ).to receive(:company_calendar_id) { company_calendar_id }
-      # allow_any_instance_of(
-      #   GoogleCalendarService
-      # ).to receive(:initialize) { calendar }
-      allow(calendar).to receive(:get_event).with(
-        company_calendar_id, id
-      ) { response }
+
+      allow(Google::APIClient::ClientSecrets).to receive(:new).and_return(client_service)
+      allow(
+        Google::Apis::CalendarV3::CalendarService
+      ).to receive(:new).and_return(google_api_calendar_service)
+      allow(client_service).to receive(:to_authorization) { signet_object }
+      allow(google_api_calendar_service).to receive(:authorization=)
+      allow(google_api_calendar_service).to receive(:authorization) { signet_object }
+      allow(signet_object).to receive(:refresh!)
+      allow(google_api_calendar_service).to receive(:get_event) { response }
+    end
+    after do
+      GoogleCalendarService.new(user)
     end
     context 'when service is up,' do
       let!(:response) { double('response', id: id) }
-      before do
-      end
+      it { expect(Google::APIClient::ClientSecrets).to receive(:new) }
+      it { expect(Google::Apis::CalendarV3::CalendarService).to receive(:new) }
+      it { expect(client_service).to receive(:to_authorization) }
+      it { expect(google_api_calendar_service).to receive(:authorization=) }
+      it { expect(google_api_calendar_service).to receive(:authorization) }
+      it { expect(signet_object).to receive(:refresh!) }
+      # it { expect(GoogleCalendarService.new(user)).to eq(google_api_calendar_service) }
       it 'existing_event should have the same id' do
-        skip 'for a short time'
-        # VCR.use_cassette('whatever cassette name you want') do
-          expect(GoogleCalendarService.new(user).existing_event?(id)).to be true
-        # end
+        expect(GoogleCalendarService.new(user).existing_event?(id)).to be true
       end
     end
 
-
     context 'when service is down,' do
       let(:response) { double('response', id: "nil") }
-      before do
-        allow(calendar).to receive(:get_event).with(
-          company_calendar_id, id
-        ) { response }
-      end
       it 'existing_event should  not find the same id' do
-        skip 'for a short time'
-        expect(gs.existing_event(id)).to be false
+        expect(GoogleCalendarService.new(user).existing_event?(id)).to be false
       end
     end
   end
