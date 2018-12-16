7b29592adbed9527bf6b40d1d82bd6870526a1f9
Etienne WEIL <weil.etienne@hotmail.fr>
2018-12-16 18:30:25 +0100

    site conf for redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis//localhost:6379/0', password: ENV['REDIS_PWD'] }
+  config.redis = { url: "redis//#{ENV['SITE']}:6379/0", password: ENV['REDIS_PWD'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis//localhost:6379/0', password: ENV['REDIS_PWD'] }
+  config.redis = { url: "redis//#{ENV['SITE']}:6379/0", password: ENV['REDIS_PWD'] }
 end
