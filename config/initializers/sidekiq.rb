afdddc33f6b184297c7a8f37c6320ea47a0559d0
Etienne WEIL <fitchMitch@users.noreply.github.com>
2018-12-16 22:22:19 +0100

    Feat/polls reminder (#39)
    * Sidekiq service start. Needs a deployment in order to start the service.
    
    * temporarily for tests to pass, get back to deliver_now
    
    * with redis password set
    
    * a fake job when showing a user
    
    * site conf for redis
    
    * fixing the redis url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis//#{ENV['SITE']}:6379/0", password: ENV['REDIS_PWD'] }
+  # config.redis = { db: 1 } # this is working localy
+  config.redis = { url: "redis://#{ENV['SITE']}:6379/0", password: ENV['REDIS_PWD'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis//#{ENV['SITE']}:6379/0", password: ENV['REDIS_PWD'] }
+  # config.redis = { db: 1 } # this is working locally
+  config.redis = { url: "redis://#{ENV['SITE']}:6379/0", password: ENV['REDIS_PWD'] }
 end
