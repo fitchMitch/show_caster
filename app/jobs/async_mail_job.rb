23a4dddc2c6650e2a6069bd053ce63d70f80879a
Etienne WEIL <fitchMitch@users.noreply.github.com>
2018-12-16 16:56:19 +0100

    Feat/polls reminder (#37)
    * Sidekiq service start. Needs a deployment in order to start the service.
    * temporarily for tests to pass, get back to deliver_now
    * with redis password set
    * a fake job when showing a user

---
+++ app/jobs/async_mail_job.rb
@@ -0,0 +1,10 @@
+class AsyncMailJob < ApplicationJob
+  include LoggingHelper
+  queue_as :default
+
+  def perform(*args)
+    info_logging("message start")
+    sleep 10
+    info_logging("message end")
+  end
+end
