b453f556c6170801400ce36ff3cfb770a6613d86
Etienne WEIL <weil.etienne@hotmail.fr>
2018-12-20 13:41:00 +0100

    final configuration is ok https://goo.gl/KFoKyn

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,16 @@
 Sidekiq.configure_server do |config|
-  # config.redis = { db: 1 } # this is working localy
-  config.redis = { url: "redis://#{ENV['SITE']}:6379/0", password: ENV['REDIS_PWD'] }
+  # config.redis = { db: 1 } # works localy
+  config.redis = {
+    url: "redis://#{ENV['SITE']}:6379/0",
+    password: ENV['REDIS_PWD']
+  }
 end
 
 Sidekiq.configure_client do |config|
-  # config.redis = { db: 1 } # this is working locally
-  config.redis = { url: "redis://#{ENV['SITE']}:6379/0", password: ENV['REDIS_PWD'] }
+  # config.redis = { db: 1 } # works localy
+  config.redis = {
+    url: "redis://#{ENV['SITE']}:6379/0",
+    password: ENV['REDIS_PWD']
+  }
+  #localy REDIS has no password but redis is set with norequiredpassword option
 end
