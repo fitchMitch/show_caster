ff6e3040cb3f2dde164433868445ffe067d2f46f
Etienne WEIL <weil.etienne@hotmail.fr>
2018-12-22 21:34:43 +0100

    have travis work with sidekiq and redis

--- app/jobs/sms_sender_job.rb
+++ app/jobs/sms_sender_job.rb
@@ -1,10 +1,1 @@
-class SmsSenderJob < ApplicationJob
-  include LoggingHelper
-  queue_as :default
 
-  def perform(*args)
-    info_logging("message start")
-    sleep 10
-    info_logging("message end")
-  end
-end
