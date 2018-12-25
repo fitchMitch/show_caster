8c56e68db5f4fd45c5a55918754cafbec97a5c42
Etienne WEIL <fitchMitch@users.noreply.github.com>
2018-12-25 15:25:09 +0100

    polls update and destroy notifications (#45)
    * polls update and destroy notifications and travis.yml

--- app/services/notification_service.rb
+++ app/services/notification_service.rb
@@ -1,30 +1,68 @@
+require 'sidekiq/api'
+
 class NotificationService
   include LoggingHelper
   @@too_short_notice_days = 2.days
 
-  def self.too_short_notice_days
-    @@too_short_notice_days
-  end
-
   def self.poll_creation(poll)
     PollMailer.poll_creation_mail(poll).deliver_now
 
-    day_gap = Poll.days_threshold_for_first_mail_alert.days
-    poll_end_delay = poll.expiration_date - Time.zone.now
-    reminder_delay = poll_end_delay - day_gap.to_i
+    poll_end_delay, reminder_delay = NotificationService.get_delays(poll)
     return nil if self.short_notice(reminder_delay)
 
-    ReminderMailJob.set(wait: reminder_delay.seconds).perform_later(poll.id)
-    ReminderPollEndJob.set(wait: poll_end_delay.seconds).perform_later(poll.id)
+    NotificationService.set_future_mail_notifications(poll)
   end
 
-  def self.short_notice(delay)
-    delay < NotificationService.too_short_notice_days.to_i
+  def self.poll_notifications_update(poll)
+    poll_changes = NotificationService.analyse_poll_changes(poll)
+    # Poll changes should be noticed to adminstrators and owner only
+    # TODO with analyse_poll_changes
+    # identify which changes there were and which one are important
+    NotificationService.destroy_all_notifications(poll)
+    return nil if poll_changes.fetch("expiration_date", nil).nil?
+
+    NotificationService.set_future_mail_notifications(poll)
+  end
+
+  def self.set_future_mail_notifications(poll)
+    poll_end_delay, reminder_delay = NotificationService.get_delays(poll)
+    # for some player to remember they should answer poll's question
+    ReminderMailJob.set(wait: reminder_delay.seconds).perform_later(poll.id)
+    # for some poll's owner to remember they should announce the end of the poll
+    ReminderPollEndJob.set(wait: poll_end_delay.seconds).perform_later(poll.id)
   end
   # ========= Jobs are now set =====================
 
 
   # ========= Jobs launch the following services =====================
+  def self.destroy_all_notifications(poll)
+    scheduled_jobs = Sidekiq::ScheduledSet.new
+    scheduled_jobs.each do |job|
+      unless job['args'].empty?
+        job.delete if job['args'].first['arguments'] == [poll.id]
+      end
+    end
+    # @item={
+    # "class"=>"ActiveJob::QueueAdapters::SidekiqAdapter::JobWrapper",
+    # "wrapped"=>"ReminderPollEndJob",
+    # "queue"=>"mailers",
+    # "args"=>
+    #   [
+    #     {
+    #       "job_class"=>"ReminderPollEndJob",
+    #       "job_id"=>"2db0c540-6bf3-49b2-b496-db22733b80bf",
+    #       "provider_job_id"=>nil,
+    #       "queue_name"=>"mailers",
+    #       "priority"=>nil,
+    #       "arguments"=>[39],
+    #       "locale"=>"fr"
+    #     }
+    #   ],
+    # "retry"=>true,
+    # "jid"=>"a9decad10eb3374c7d74276a",
+    # "created_at"=>1545506938.8081102}
+  end
+
   def self.poll_reminder_mailing(poll_id)
     poll = Poll.find(poll_id)
     return nil if poll.nil? || poll.missing_voters_ids.empty?
@@ -43,5 +81,52 @@ class NotificationService
   rescue StandardError => e
     Bugsnag.notify(e)
     warn_logging('poll_end_reminder_mailing failure') { puts e }
+  end
+
+  # ============ These are just helpers =======================
+  def self.too_short_notice_days
+    @@too_short_notice_days
+  end
+
+  def self.short_notice(delay)
+    delay < NotificationService.too_short_notice_days.to_i
+  end
+
+  def self.analyse_poll_changes(poll)
+    # Sample
+    # {
+    #   "expiration_date"=>[
+    #     Tue, 01 Jan 2019 00:00:00 CET +01:00,
+    #     Wed, 02 Jan 2019 00:00:00 CET +01:00
+    #   ],
+    #   "question"=>[
+    #     "Question deuxxx",
+    #     "Question deux"
+    #   ],
+    #   "answer_changes"=>[
+    #     {
+    #       "date_answer"=>[
+    #         Wed, 26 Dec 2018 19:00:00 CET +01:00,
+    #         Wed, 26 Dec 2018 18:00:00 CET +01:00
+    #       ],
+    #       "updated_at"=>[
+    #         Mon, 24 Dec 2018 10:26:40 CET +01:00,
+    #         Mon, 24 Dec 2018 10:29:17 CET +01:00
+    #       ]
+    #     },
+    #     {}
+    #   ]
+    # }
+    answer_changes = []
+    poll.answers.each { |answer| answer_changes << answer.previous_changes }
+    poll.previous_changes.merge('answer_changes' => answer_changes)
+  end
+
+  def self.get_delays(poll)
+    poll_end_delay = poll.expiration_date - Time.zone.now
+    day_gap = Poll.days_threshold_for_first_mail_alert.days
+    reminder_delay = poll_end_delay - day_gap.to_i
+
+    [poll_end_delay, reminder_delay]
   end
 end
--- spec/services/notification_service_spec.rb
+++ spec/services/notification_service_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+
 RSpec.describe NotificationService, type: :service do
   describe '.poll_creation,' do
     let(:poll_to_deliver) { double('poll_to_deliver')}
@@ -14,6 +15,7 @@ RSpec.describe NotificationService, type: :service do
       let(:performed_later) { double('performed_later') }
       let(:end_to_be_performed) { double('end_to_be_performed') }
       let(:end_performed_later) { double('end_performed_later') }
+
       describe 'it first' do
         before do
           allow(ReminderMailJob).to receive(:set) { to_be_performed }
@@ -32,6 +34,7 @@ RSpec.describe NotificationService, type: :service do
           subject
         end
       end
+
       describe 'it then' do
         before do
           allow(ReminderMailJob).to receive(:set) { to_be_performed }
@@ -57,7 +60,6 @@ RSpec.describe NotificationService, type: :service do
   end
 
   describe '.poll_reminder_mailing' do
-
     subject(:mailing) { described_class.poll_reminder_mailing(poll_id) }
 
     context 'when poll no longer exists' do
@@ -80,11 +82,173 @@ RSpec.describe NotificationService, type: :service do
     context 'some have not voted this still existing poll' do
       let!(:poll_opinion) { create(:poll_opinion) }
       let!(:poll_id) { poll_opinion.id }
-      let(:a_mail) { double('a_mail')}
+      let(:a_mail) { double('a_mail') }
       before do
-        allow(PollMailer).to receive(:poll_reminder_mail).with(poll_opinion) { a_mail }
+        allow(PollMailer).to receive(
+          :poll_reminder_mail
+        ).with(poll_opinion) { a_mail }
       end
       it { expect(mailing).to eq a_mail }
     end
   end
+
+  describe '.poll_notifications_update' do
+    subject { described_class.poll_notifications_update(poll_date) }
+    context 'when expiration_date does not change' do
+      let(:poll_date) { build(:poll_date) }
+      before do
+        allow(NotificationService).to receive(:analyse_poll_changes) { {} }
+      end
+      it { expect(subject).to be nil }
+    end
+
+    context 'when expiration_date does not change' do
+      let(:poll_date) { build(:poll_date) }
+      let(:job_done) { double('job_done') }
+      before do
+        allow(NotificationService).to receive(:analyse_poll_changes) do
+          {
+            'expiration_date' => [
+              Time.zone.now + 2.days,
+              Time.zone.now + 3.days
+            ]
+          }
+        end
+      end
+      it 'should destroy old notifications' do
+        expect(NotificationService).to receive(:destroy_all_notifications).with(poll_date)
+        expect(NotificationService).to receive(
+          :set_future_mail_notifications
+        ).with(poll_date)
+        subject
+      end
+    end
+  end
+
+  describe '.set_future_mail_notifications' do
+    subject { described_class.set_future_mail_notifications(poll) }
+    let!(:poll) { build(:poll_date) }
+
+    let(:mailjob) { double('mailjob') }
+    let(:mailendjob) { double('mailendjob') }
+    before :each do
+      allow(mailjob).to receive(:perform_later)
+      allow(mailendjob).to receive(:perform_later)
+      allow(described_class).to receive(:get_delays) { [2, 1] }
+    end
+    it { expect(ReminderMailJob).to receive(:set).with({ wait: 1 }) { mailjob } }
+    it { expect(ReminderPollEndJob).to receive(:set).with({ wait: 2 }) { mailendjob } }
+    after :each do
+      subject
+    end
+  end
+
+  describe '.destroy_all_notifications' do
+    let!(:poll_id) { 123 }
+    let!(:poll) { build(:poll_date, id: poll_id) }
+    let(:scheduled_jobs) { [a_scheduled_job] }
+    let(:arguments_array) { double('arguments_array', { 'arguments' => [poll_id]  }) }
+    let(:a_scheduled_job) { double('a_scheduled_job', { 'args' => [arguments_array] }) }
+      # {
+      # 'class'=>'ActiveJob::QueueAdapters::SidekiqAdapter::JobWrapper',
+      # 'wrapped'=>'ReminderPollEndJob',
+      # 'queue'=>'mailers',
+      # 'args'=> [
+      #   {
+      #     'job_class'=>'ReminderPollEndJob',
+      #     'job_id'=>'2db0c540-6bf3-49b2-b496-db22733b80bf',
+      #     'provider_job_id'=>nil,
+      #     'queue_name'=>'mailers',
+      #     'priority'=>nil,
+      #     'arguments'=>[poll.id],
+      #     'locale'=>'fr'
+      #   }
+      # ],
+      # 'retry'=>true,
+      # 'jid'=>'a9decad10eb3374c7d74276a',
+      # 'created_at'=>1545506938.8081102
+      # }
+    before do
+      allow(Sidekiq::ScheduledSet).to receive(:new) { scheduled_jobs }
+      allow(a_scheduled_job).to receive(:delete)
+      allow(scheduled_jobs.first).to receive(:delete)
+    end
+    it 'should delete the related notifications' do
+      skip 'missing test due  to double not being a correct object'
+      expect(a_scheduled_job).to receive(:delete) {}
+      described_class.destroy_all_notifications(poll)
+    end
+  end
+
+  describe '.analyse_poll_changes' do
+    subject { described_class.analyse_poll_changes(poll) }
+    context 'with a question change' do
+      let(:poll) { build(:poll_date) }
+      let(:question_change) { { 'question' => ['Question deuxxx', 'Question deux']} }
+      before do
+        allow(poll).to receive(:previous_changes) { question_change }
+      end
+      it { expect(subject).to eq question_change.merge({ 'answer_changes'=>[] }) }
+    end
+    context 'with a question change in the answers' do
+      let!(:answer_opinion) { create(:answer_opinion) }
+      let(:poll) { answer_opinion.poll_opinion }
+      let(:answer_change) do
+        {
+          'answer_label'=>[
+            'Answer 1',
+            'Answer 1 altered'
+          ],
+          'updated_at'=>[
+            Time.zone.now,
+            Time.zone.now
+          ]
+        }
+      end
+      let(:result) { { 'answer_changes'=>[answer_change] } }
+      before do
+        allow(poll).to receive(:previous_changes) { {} }
+        allow_any_instance_of(Answer).to receive(:previous_changes) { answer_change }
+      end
+      it { expect(subject).to eq result }
+    end
+  end
+
+  describe '.get_delays' do
+    subject { described_class.get_delays(poll_date) }
+    context 'when expiration_date is set two weeeks away from current date' do
+      let(:poll_date) { build(:poll_date, expiration_date: Time.zone.now + 2.weeks) }
+      it { expect(subject[0]).to be > 0 }
+      it { expect(subject[1]).to be > 0 }
+      it { expect(subject[0]).to be > subject[1] }
+      it { expect(subject[0]).to be > Poll.days_threshold_for_first_mail_alert.days.to_i }
+    end
+    context 'when expiration_date is set one day away from current date' do
+      let(:poll_date) { build(:poll_date, expiration_date: Time.zone.now + 1.day) }
+      it { expect(subject[0]).to be > 0 }
+      it { expect(subject[1]).to be < 0 }
+      it { expect(subject[0]).to be > subject[1] }
+      it { expect(subject[0]).to be < Poll.days_threshold_for_first_mail_alert.days.to_i }
+    end
+  end
 end
+
+# {
+# 'class'=>'ActiveJob::QueueAdapters::SidekiqAdapter::JobWrapper',
+# 'wrapped'=>'ReminderPollEndJob',
+# 'queue'=>'mailers',
+# 'args'=> [
+#   {
+#     'job_class'=>'ReminderPollEndJob',
+#     'job_id'=>'2db0c540-6bf3-49b2-b496-db22733b80bf',
+#     'provider_job_id'=>nil,
+#     'queue_name'=>'mailers',
+#     'priority'=>nil,
+#     'arguments'=>[poll.id],
+#     'locale'=>'fr'
+#   }
+# ],
+# 'retry'=>true,
+# 'jid'=>'a9decad10eb3374c7d74276a',
+# 'created_at'=>1545506938.8081102
+# }
