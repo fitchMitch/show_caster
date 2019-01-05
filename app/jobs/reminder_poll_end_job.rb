cc1625de406b9d9705c39781dc53e87629ad9da5
Etienne WEIL <weil.etienne@hotmail.fr>
2019-01-05 18:44:28 +0100

    removing some trace

--- app/jobs/reminder_poll_end_job.rb
+++ app/jobs/reminder_poll_end_job.rb
@@ -3,9 +3,9 @@ class ReminderPollEndJob < ApplicationJob
   queue_as :mailers
 
   def perform(poll_id)
-    ReminderPollEndJob.debug_logging('before NotificationService')
+    # ReminderPollEndJob.debug_logging('before NotificationService')
     NotificationService.poll_end_reminder_mailing(poll_id)
-    ReminderPollEndJob.debug_logging('after NotificationService')
+    # ReminderPollEndJob.debug_logging('after NotificationService')
   rescue StandardError => e
     Bugsnag.notify(e)
     ReminderPollEndJob.error_logging(e)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'www.les-sesames.fr' # Don't use this literally; use your local dev host instead
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: "http://#{host}" }
 
   ActionMailer::Base.smtp_settings = {
     address:         ENV['SMTP_SERVER'],
