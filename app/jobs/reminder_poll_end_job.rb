d7f3a468f3e9aa5767df4bf9c886461479fac06a
Etienne WEIL <weil.etienne@hotmail.fr>
2019-01-05 18:56:56 +0100

    moving PollMailer call inside the job

--- app/jobs/reminder_poll_end_job.rb
+++ app/jobs/reminder_poll_end_job.rb
@@ -4,10 +4,19 @@ class ReminderPollEndJob < ApplicationJob
 
   def perform(poll_id)
     # ReminderPollEndJob.debug_logging('before NotificationService')
-    NotificationService.poll_end_reminder_mailing(poll_id)
+    #NotificationService.poll_end_reminder_mailing(poll_id)
     # ReminderPollEndJob.debug_logging('after NotificationService')
+    poll = Poll.find(poll_id)
+    if poll.nil?
+      Rails.logger.warn("This poll cannot be sent since it no longer exists")
+      return nil
+    end
+
+    PollMailer.poll_end_reminder_mail(poll).deliver_later
   rescue StandardError => e
     Bugsnag.notify(e)
-    ReminderPollEndJob.error_logging(e)
+    Rails.logger.error("poll_end_reminder_mailing failure: #{e}")
+    # NotificationService.error_logging("poll_end_reminder_mailing failure: #{e}")
+    raise e
   end
 end
