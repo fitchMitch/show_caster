71e55df8375971305139dea78ebc13ad9b64b24d
Etienne WEIL <weil.etienne@hotmail.fr>
2019-01-06 21:34:14 +0100

    filling job with direct PollMailer call

--- app/jobs/reminder_poll_end_job.rb
+++ app/jobs/reminder_poll_end_job.rb
@@ -5,11 +5,26 @@ class ReminderPollEndJob < ApplicationJob
   def perform(poll_id)
     # ReminderPollEndJob.debug_logging('before NotificationService')
     Rails.logger.debug(" right before ReminderPollEndJob ")
-    NotificationService.poll_end_reminder_mailing(poll_id)
-    # ReminderPollEndJob.debug_logging('after NotificationService')
+    # NotificationService.poll_end_reminder_mailing(poll_id)
+    poll = Poll.find(poll_id)
+    Rails.logger.debug "***** Just before poll's test *****"
+    if poll.nil?
+      Rails.logger.warn("This poll cannot be sent since it no longer exists")
+      return nil
+    end
+    Rails.logger.debug "***** Just before PollMailer.poll_end_reminder_mail call *****"
+
+    PollMailer.poll_end_reminder_mail(poll).deliver_now
   rescue StandardError => e
     Bugsnag.notify(e)
-    Rails.logger.error("ReminderPollEndJob error with: #{e}")
-    # ReminderPollEndJob.error_logging(e)
+    Rails.logger.error("poll_end_reminder_mailing failure: #{e}")
+    # NotificationService.error_logging("poll_end_reminder_mailing failure: #{e}")
+    raise e
+
+    # ReminderPollEndJob.debug_logging('after NotificationService')
+  # rescue StandardError => e
+  #   Bugsnag.notify(e)
+  #   Rails.logger.error("ReminderPollEndJob error with: #{e}")
+  #   # ReminderPollEndJob.error_logging(e)
   end
 end
