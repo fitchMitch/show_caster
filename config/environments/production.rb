27b03ebf62af0d549886afb6a4554d1facdc205c
Etienne WEIL <weil.etienne@hotmail.fr>
2019-01-08 22:43:46 +0100

    using ActionMailer::Base.delivery_method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,10 +56,10 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
   host = 'les-sesames.fr' # Don't use this literally; use your local dev host instead
   config.action_mailer.default_url_options = { host: host }
 
+  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address:                ENV['SMTP_SERVER'],
     port:                   587,
