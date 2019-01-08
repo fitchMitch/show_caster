bec66b9d22131e566d9969ed883c0c8cc7a95611
Etienne WEIL <weil.etienne@hotmail.fr>
2019-01-08 16:50:12 +0100

    more traces

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,7 +104,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :warn
+  config.log_level = :debug
 
   # add time to lograge
   config.lograge.enabled = true
