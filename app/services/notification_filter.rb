227371acb3568573420da1ed4a8066c192b80aa5
Etienne WEIL <weil.etienne@hotmail.fr>
2019-01-15 18:14:57 +0100

    back to normal

--- app/services/notification_filter.rb
+++ app/services/notification_filter.rb
@@ -15,7 +15,7 @@ class NotificationFilter < Notification
     poll = Poll.find_by(id: poll_id)
     return nil if poll.nil?
 
-    PollMailer.poll_end_reminder_mail(poll).deliver_now!
+    PollMailer.poll_end_reminder_mail(poll).deliver_now
   rescue StandardError => e
     # Bugsnag.notify(e)
     Rails.logger.error("poll_end_reminder_mailing failure: #{e}")
